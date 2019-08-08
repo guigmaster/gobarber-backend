8d7b083b8b379071342a55c8a53a286d5585db17
Guilherme Silva <guisilva.master@gmail.com>
2019-08-08 20:40:33 -0300

    List of providers, with file static serve

--- src/routes.js
+++ src/routes.js
@@ -8,16 +8,19 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('users', UserController.store);
-routes.post('sessions', SessionController.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post('users', UserController.update);
-routes.post('files', upload.single('file'), FileController.store);
+routes.post('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
