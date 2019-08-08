0e828605a2e3372039134d635868b8ae99bf1e88
Guilherme Silva <guisilva.master@gmail.com>
2019-08-08 19:36:37 -0300

    Create FileController and model for file upload

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('users', UserController.store);
 routes.post('sessions', SessionController.store);
@@ -13,5 +18,6 @@ routes.post('sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('users', UserController.update);
+routes.post('files', upload.single('file'), FileController.store);
 
 export default routes;
