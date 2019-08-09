7b023cf13ce2f9f0d38a57b429ccf78ae2b84d04
Guilherme Silva <guisilva.master@gmail.com>
2019-08-09 19:36:19 -0300

    List and update notifications

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleControler from './app/controllers/ScheduleControler';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,5 +30,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedules', ScheduleControler.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
