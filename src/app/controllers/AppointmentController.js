ad3e832ddabe14453a2f067644500e809a48b719
Guilherme Silva <guisilva.master@gmail.com>
2019-08-09 20:05:12 -0300

    Send a email on canceled appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,6 +7,7 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -113,7 +114,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -131,6 +140,12 @@ class AppointmentController {
 
     appointment.canceled_at = new Date();
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.povider.name} <${appointment.povider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você possui um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
