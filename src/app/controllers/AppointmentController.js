6e4968c4d232f74dd32776ea533d1113cefac0ef
Guilherme Silva <guisilva.master@gmail.com>
2019-08-09 22:16:02 -0300

    Create and configure queue of background jobs

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,9 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -146,18 +148,7 @@ class AppointmentController {
     appointment.canceled_at = new Date();
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.povider.name} <${appointment.povider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "dd 'de' MMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,29 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale';
+
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    await Mail.sendMail({
+      to: `${appointment.povider.name} <${appointment.povider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(parseISO(appointment.date), "dd 'de' MMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
